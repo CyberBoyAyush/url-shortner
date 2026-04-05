e3b5f07a3ac105a3e5e6af4cbebe9863426a3cab
cyberboyayush <hi@aysh.me>
2026-04-05 11:20:00 +0530

    Add Jest setup and initial test structure

---
+++ tests/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('URL Shortener API', () => {
+  beforeEach(() => {
+    // Tests will go here
+  });
+
+  describe('POST /shorten', () => {
+    test('should return a short code for a valid URL', async () => {
+      // Test implementation coming next
+    });
+  });
+});
